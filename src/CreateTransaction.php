697aa909fc61bcb5acfaba7617b160725697b904
Chhuor Ich Him <chhuor.ichhim@sabay.com>
2019-01-02 17:39:01 +0700

    Add Payway and CreateTransaction class

---
+++ src/CreateTransaction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace IchHim;
+
+class CreateTransaction extends Payway
+{
+    /**
+     * @param string $reference_id
+     * @param float  $amount
+     * @param array  $items
+     *
+     * @return string
+     */
+    public function getHash(string $reference_id, float $amount, array $items = [])
+    {
+        $data = $this->getMerchantID() . $reference_id . $amount;
+
+        if (count($items)) {
+            $items = json_encode($items);
+
+            $data .= $items;
+        }
+
+        return $this->makeHash($data);
+    }
+}
---
+++ src/Payway.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace IchHim;
+
+class Payway
+{
+    /**
+     * The `merchant_id` credential that got from ABA.
+     *
+     * @var string
+     */
+    protected $merchant_id;
+
+    /**
+     * The `api_key` credential that got from ABA.
+     *
+     * @var string
+     */
+    protected $api_key;
+
+    /**
+     * @param string $merchant_id
+     * @param string $api_key
+     */
+    public function __construct($merchant_id, $api_key)
+    {
+        $this->merchant_id = $merchant_id;
+        $this->api_key     = $api_key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMerchantID()
+    {
+        if (isset($this->merchant_id)) {
+            return $this->merchant_id;
+        }
+
+        throw new \Exception("`merchant_id` key must exists and not null.");
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        if (isset($this->api_key)) {
+            return $this->api_key;
+        }
+
+        throw new \Exception("`api_key` key must exists and not null.");
+    }
+
+    /**
+     * @param string $data
+     *
+     * @return string
+     */
+    protected function makeHash($data)
+    {
+        $hash = hash_hmac('sha512', $data, $this->getApiKey(), true);
+
+        return base64_encode($hash);
+    }
+}
